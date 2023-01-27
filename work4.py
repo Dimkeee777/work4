af60d54caf05f32d89a35a737ff8fd1edba4cefd
Dimkeee777 <lbtumatym1@gmail.com>
2023-01-28 01:46:05 +0300

    Add work4.py

---
+++ work4.py
@@ -0,0 +1,35 @@
+# number = 5
+# #
+# # print('Результат', number)
+# #
+# #
+# # digit = 4.5
+# # word = 'Result'
+# #
+# # boolean = False
+# #
+# # print(boolean)
+# # print(number, digit, word)
+# #
+# # str_num = "5"
+# #
+# # print(number + int(str_num))
+
+num1 = int(input('введите первое число: '))
+num2 = int(input('введите второе число: '))
+
+num1 += 5
+
+print('Result:', num1 + num2)
+print('Result:', num1 / num2)
+print('Result:', num1 - num2)
+print('Result:', num1 * num2)
+print('Result:', num1 ** num2)
+print('Result:', num1 // num2)
+
+
+word = "hi"
+
+print(word * 2)
+
+word = True
